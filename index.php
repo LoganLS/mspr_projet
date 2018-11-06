42dfbe38aed85da600c9e0d8f7981cea9ff37b6a
Chloé <chloe.gautreau@ecoles-wis.net>
2018-11-06 16:59:33 +0100

    page accueil php

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
     $title='Accueil';
     $description='';
 
+    $sql= 'SELECT id, name, picture
+    FROM events
+    ORDER BY date_start
+    LIMIT 5';
+
+    $stmt=$conn->prepare($sql);
+    $stmt->execute();
+    $evenements=$stmt->fechtAll();
+
+
     include_once('php/get_all_events_published.php');
 ?>
 <?php include_once('layouts/header.php');?>
@@ -10,6 +20,7 @@
 
 </main>
 <?php include_once('layouts/footer.php');?>
+
 
 
 <!DOCTYPE html>
@@ -21,8 +32,16 @@
   </head>
   <body>
 
+
     <h1>EVENEMENTS NANTES</h1>
     <h2>Spectacles, concerts, festivals et sorties</h2>
+    <?php
+foreach ($evenements as $evenement { ?>
+<img src="images/<?php echo $evenement->picture ?>" alt="" id="<?php echo $evenement->name ?>" onclick="location.href='event_detail.php?id_event=<?php echo $evenement->id ?>'">
+
+<?php }
+ ?>
+
 
 
   </body>
