39f8f48b1b1a205c9e3f25ecfdbe69953e0d1fe6
LoganLS <logan_56@hotmail.fr>
2018-11-07 12:22:44 +0100

    changements css

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,6 +1,6 @@
         <footer>
             <div id="texte_footer">
-
+                Projet MSPR WIS 2 - Logan LE SAUX - Laurine MALLET - Chloé GAUTREAU
             </div>
         </footer>
     </body>
