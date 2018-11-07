fd7a7728bfb6ab0f68fa59350d01d6da9eed03d9
Chloé <chloe.gautreau@ecoles-wis.net>
2018-11-07 17:01:50 +0100

    modification php modification profil

--- php/modifier_profil.php
+++ php/modifier_profil.php
@@ -4,14 +4,14 @@ include_once('db.php');
 die();
 $first_name=$_POST['first_name'];
 $last_name=$_POST['last_name'];
-$email=_$POST['email'];
+$email=$_POST['email'];
 $username=$_POST['username'];
 $date_birthday=$_POST['date_birthday'];
 $password=nd5($_POST['password']);
 
 
 $sql='UPDATE users SET first_name=:first_name,last_name=:last_name,date_birthday=:date_birthday,username=:username,email=:email,password=:password
- ';
+ WHERE id=:id';
  $stmt=$conn->prepare($sql);
  $stmt->bindValue("first_name",$first_name);
  $stmt->bindValue("last_name",$last_name);
@@ -19,6 +19,9 @@ $sql='UPDATE users SET first_name=:first_name,last_name=:last_name,date_birthday
  $stmt->bindValue("username",$username);
  $stmt->bindValue("date_birthday",$date_birthday);
  $stmt->bindValue("password",$password);
+ $stmt->bindValue("id",$_SESSION['id']);
 
  $stmt->execute();
+header('Location:../profil.php');
+
  ?>
