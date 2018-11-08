088f96c0f64cecb72a8c9ae625d3fcb9b25acd18
Chloé <chloe.gautreau@ecoles-wis.net>
2018-11-08 14:57:40 +0100

    changement footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,7 +1,15 @@
         <footer>
+          <div id="texte_footer2">
+              Retrouvez nous sur nos réseaux sociaux :
+          </div>
+          <div id="logo_reseaux">
+            <img src="images/logos-reseaux-sociaux.png" alt="logo reseaux sociaux">
+          </div>
+
             <div id="texte_footer">
                 Projet MSPR WIS 2 - Logan LE SAUX - Laurine MALLET - Chloé GAUTREAU
             </div>
+
         </footer>
     </body>
     <?php include('layouts/javascript.php');?>
