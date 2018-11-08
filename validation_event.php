76be7a7b428f79dd269984f0511fcc25636c9d7f
laurinemallet <laurinem12@hotmail.com>
2018-11-08 15:52:02 +0100

    validation events

--- validation_event.php
+++ validation_event.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
     session_start();
     $title='Espace administratif';
     $description='';
 ?>
-<?php 
+<?php
 include_once('php/get_all_events_not_published.php');
 include_once('layouts/header.php');?>
 <main class="container">
     <h1>Espace administratif : Validation d'un événement</h1>
     <?php
     if(empty($events)){
-        echo '<div>Aucun événément à valider actuellement.</div>';
+        echo '<div>Aucun événément a valider actuellement.</div>';
     }else{
         echo '<ul class="list-group">';
             foreach($events as $event){ ?>
                 <li class="list-group-item">
                     <a href="event_detail_admin.php?id=<?php echo $event->id;?>"><?php echo $event->name;?></a>
                 </li>
-            <?php } 
+            <?php }
         echo '</ul>';
     } ?>
 </main>
