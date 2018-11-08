6c530e275df9ed0c93223fcaaada8f52489136dc
Chloé <chloe.gautreau@ecoles-wis.net>
2018-11-08 14:20:56 +0100

    typo

--- layouts/head.php
+++ layouts/head.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link rel='stylesheet' href='https://fullcalendar.io/releases/fullcalendar/3.9.0/fullcalendar.min.css'>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-<link href="http://fr.allfont.net/allfont.css?fonts=neon" rel="stylesheet" type="text/css" />
+<link href="http://fr.allfont.net/allfont.css?fonts=neon" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/header.css">
 <link rel="stylesheet" href="css/accueil.css">
