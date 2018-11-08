45201b8ae4816c7d24a4314686dc57f0ed729641
LoganLS <logan_56@hotmail.fr>
2018-11-08 14:17:13 +0100

    verification utilisateur existe

--- profil.php
+++ profil.php
@@ -4,9 +4,6 @@ include("php/db.php");
     session_start();
     $title='Espace profil';
     $description='';
-
-    include_once('php/get_informations_user.php');
-
 ?>
 <?php include_once('layouts/header.php');?>
 <main>
@@ -14,79 +11,89 @@ include("php/db.php");
 
 
     <h1 class="h2">Espace profil</h1>
-      <p id="nom_prenom_profil"><?php echo $member->first_name." ".$member->last_name ?></p>
+    <?php
+      
+      include_once('php/verification_user_existant.php');
+      
+      if($utilisateurExiste===1){
+        include_once('php/get_informations_user.php');?>
+      
+        <p id="nom_prenom_profil"><?php echo $member->first_name." ".$member->last_name ?></p>
 
-  <table class="table table-bordered table-striped table_page_profil">
+      <table class="table table-bordered table-striped table_page_profil">
+        <tbody>
+        <tr>
+          <td class="text_titre_info_profil">Date de naissance</td>
+          <td><?php echo date("d-m-Y",strtotime($member->date_birthday)) ?></td>
+        </tr>
+        <tr>
+          <td class="text_titre_info_profil">Pseudo</td>
+          <td><?php echo $member->username ?></td>
+        </tr>
+        <tr>
+          <td class="text_titre_info_profil">Email</td>
+          <td><?php echo $member->email ?></td>
+        </tr>
+        <tr>
+          <td class="text_titre_info_profil">Rôle</td>
+          <td><?php echo $member->role ?></td>
+        </tr>
+        <tr>
+          <td class="text_titre_info_profil">Date de création du compte</td>
+          <td><?php echo date("d-m-Y H:i:s",strtotime($member->date_created)) ?></td>
+        </tr>
+      </tbody>
+      </table>
+
+    <?php include_once('php/get_events_participate.php') ?>
+
+
+    <table class="table table-bordered table-striped table_page_profil">
+
     <tbody>
-    <tr>
-      <td class="text_titre_info_profil">Date de naissance</td>
-      <td><?php echo date("d-m-Y",strtotime($member->date_birthday)) ?></td>
+        <tr>
+      <td class="text_titre_events_profil">Événement où je participe</td>
     </tr>
-    <tr>
-      <td class="text_titre_info_profil">Pseudo</td>
-      <td><?php echo $member->username ?></td>
-    </tr>
-    <tr>
-      <td class="text_titre_info_profil">Email</td>
-      <td><?php echo $member->email ?></td>
-    </tr>
-    <tr>
-      <td class="text_titre_info_profil">Rôle</td>
-      <td><?php echo $member->role ?></td>
-    </tr>
-    <tr>
-      <td class="text_titre_info_profil">Date de création du compte</td>
-      <td><?php echo date("d-m-Y H:i:s",strtotime($member->date_created)) ?></td>
-    </tr>
-  </tbody>
-  </table>
 
-<?php include_once('php/get_events_participate.php') ?>
+    <?php foreach($events as $event){ ?>
+    <tr>
+        <td><a href="event_detail.php?id=<?php echo $event->id;?>"><?php echo $event->name; ?></a></td>
+    </tr>
+    <?php } ?>
+    </tbody>
+    </table>
+
+    <?php include_once('php/get_events_created.php') ?>
 
 
-<table class="table table-bordered table-striped table_page_profil">
+    <table class="table table-bordered table-striped table_page_profil">
 
-<tbody>
+    <tbody>
+        <tr>
+      <td class="text_titre_events_profil">Événement créés</td>
+    </tr>
+
+    <?php foreach($events as $event){ ?>
     <tr>
-  <td class="text_titre_events_profil">Événement où je participe</td>
-</tr>
+        <td><a href="event_detail.php?id=<?php echo $event->id;?>"><?php echo $event->name; ?></a></td>
+    </tr>
+    <?php } ?>
+    </tbody>
+    </table>
 
-<?php foreach($events as $event){ ?>
-<tr>
-    <td><a href="event_detail.php?id=<?php echo $event->id;?>"><?php echo $event->name; ?></a></td>
-</tr>
+    <?php
+    if(!empty($_SESSION['id'])){
+      if($_SESSION['id'] == $_GET['id']){
+
+
+     ?>
+    <button class="btn btn-outline-dark" onclick="location.href='modifier_profil.php?id=<?php echo $_SESSION['id']; ?>'" type="button">Modifier mon profil</button>
+
+    <?php  }
+    } 
+}else{ ?>
+      <div>Aucun profil ne correspond !</div>      
 <?php } ?>
-</tbody>
-</table>
-
-<?php include_once('php/get_events_created.php') ?>
-
-
-<table class="table table-bordered table-striped table_page_profil">
-
-<tbody>
-    <tr>
-  <td class="text_titre_events_profil">Événement créés</td>
-</tr>
-
-<?php foreach($events as $event){ ?>
-<tr>
-    <td><a href="event_detail.php?id=<?php echo $event->id;?>"><?php echo $event->name; ?></a></td>
-</tr>
-<?php } ?>
-</tbody>
-</table>
-
-<?php
-if(!empty($_SESSION['id'])){
-  if($_SESSION['id'] == $_GET['id']){
-
-
- ?>
-<button class="btn btn-outline-dark" onclick="location.href='modifier_profil.php?id=<?php echo $_SESSION['id']; ?>'" type="button">Modifier mon profil</button>
-
-<?php  }
-} ?>
 
 
 
